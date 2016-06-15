2094b279f7c0b101505add11c6ab861535293c56
liyajiao <18710127273@163.com>
2016-06-15 08:40:53 +0800

    登录修改1.0

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -31,13 +31,22 @@ class LoginController extends \yii\web\Controller
         echo "忘记密码";die;
     }
     public function actionRegister(){
-        echo "用户注册";die;
+        $request=Yii::$app->request;
+        $u_name=$request->post('u_name');
+        $u_pwd=$request->post('u_pwd');
+        $u_email=$request->post('u_email');
+        $re=Yii::$app->db->createCommand()->insert('we_user',['u_name'=>"$u_name",'u_pwd'=>"$u_pwd",'u_email'=>"$u_email"])->execute();
+        if($re){
+            return $this->redirectPartial(['computer/index']);
+        }else{
+            return $this->redirectPartial(['computer/register']);
+        }
     }
     /*用于登录验证*/
     public function actionProving(){
         $username = \Yii::$app->request->post('username');
         $pwd = \Yii::$app->request->post('pwd');
-        //$remember = \Yii::$app->request->post('remember')?\Yii::$app->request->post('remember'):'';
+        $remember = \Yii::$app->request->post('remember')?\Yii::$app->request->post('remember'):'';
         //var_dump($remember);die;
         $connection = \Yii::$app->db;
         $command = $connection->createCommand("SELECT * FROM we_user WHERE u_name='$username' and u_pwd='$pwd' limit 1");
@@ -47,7 +56,11 @@ class LoginController extends \yii\web\Controller
             $session->open();
             $session->set('id', $titles[0]['u_id']);
             $session->set('name', $titles[0]['u_name']);
-            echo true;
+			if($remember){
+                $session->set('pwd', $titles[0]['u_pwd']);
+                echo true;
+            }
+			echo true;
         }else{
             echo false;
         }
