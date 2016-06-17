a80cfe56c37a6374a0a522f81ed6c81900f34f16
liyajiao <18710127273@163.com>
2016-06-17 16:55:24 +0800

    1.0

--- views/rule/add.php
+++ views/rule/add.php
@@ -121,126 +121,64 @@
 							<div class="col-xs-12">
 								<!-- PAGE CONTENT BEGINS -->
 
-								<form class="form-horizontal" role="form">
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Text Field </label>
+							<form class="form-horizontal" role="form">
+								<div class="form-group">
+									<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> 规则名称 : </label>
 
-										<div class="col-sm-9">
-											<input type="text" id="form-field-1" placeholder="Username" class="col-xs-10 col-sm-5" />
-										</div>
+									<div class="col-sm-9">
+										<input type="text" id="form-field-1" placeholder="规则名称..." class="col-xs-10 col-sm-5" />
 									</div>
+								</div>
 
-									<div class="space-4"></div>
+								<div class="space-4"></div>
 
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-2"> Password Field </label>
+								<div class="form-group">
+									<label class="col-sm-3 control-label no-padding-right" for="form-field-2"> 回复类型 : </label>
 
-										<div class="col-sm-9">
-											<input type="password" id="form-field-2" placeholder="Password" class="col-xs-10 col-sm-5" />
-											<span class="help-inline col-xs-12 col-sm-7">
-												<span class="middle">Inline help text</span>
-											</span>
-										</div>
+									<div class="col-sm-9">
+										<input type="text" id="form-field-2" placeholder="回复类型..." class="col-xs-10 col-sm-5" />
 									</div>
+								</div>
 
-									<div class="space-4"></div>
+								<div class="space-4"></div>
 
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-input-readonly"> Readonly field </label>
+								<div class="form-group">
+									<label class="col-sm-3 control-label no-padding-right" for="form-input-readonly"> 触发关键字 : </label>
 
-										<div class="col-sm-9">
-											<input readonly="" type="text" class="col-xs-10 col-sm-5" id="form-input-readonly" value="This text field is readonly!" />
-											<span class="help-inline col-xs-12 col-sm-7">
-												<label class="middle">
-													<input class="ace" type="checkbox" id="id-disable-check" />
-													<span class="lbl"> Disable it!</span>
-												</label>
-											</span>
-										</div>
+									<div class="col-sm-9">
+										<input type="text" id="form-field-2" placeholder="触发关键字..." class="col-xs-10 col-sm-5" />
 									</div>
+								</div>
 
-									<div class="space-4"></div>
+								<div class="space-4"></div>
 
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-4">Relative Sizing</label>
+								<div class="form-group">
+									<label class="col-sm-3 control-label no-padding-right" for="form-field-4">添加回复内容 : </label>
 
-										<div class="col-sm-9">
-											<input class="input-sm" type="text" id="form-field-4" placeholder=".input-sm" />
-											<div class="space-2"></div>
-
-											<div class="help-block" id="input-size-slider"></div>
-										</div>
+									<div class="col-sm-9">
+										<textarea name="" rows="5" cols="14" id="form-field-2" class="col-xs-10 col-sm-5" placeholder="添加回复内容..."></textarea>
 									</div>
+								</div>
+								
+								<div class="space-4"></div>
 
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-5">Grid Sizing</label>
+								<div class="clearfix form-actions">
+									<div class="col-md-offset-3 col-md-9">
+										<button class="btn btn-info" type="button">
+											<i class="icon-ok bigger-110"></i>
+											Submit
+										</button>
 
-										<div class="col-sm-9">
-											<div class="clearfix">
-												<input class="col-xs-1" type="text" id="form-field-5" placeholder=".col-xs-1" />
-											</div>
-
-											<div class="space-2"></div>
-
-											<div class="help-block" id="input-span-slider"></div>
-										</div>
+										&nbsp; &nbsp; &nbsp;
+										<button class="btn" type="reset">
+											<i class="icon-undo bigger-110"></i>
+											Reset
+										</button>
 									</div>
-
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right">Input with Icon</label>
-
-										<div class="col-sm-9">
-											<span class="input-icon">
-												<input type="text" id="form-field-icon-1" />
-												<i class="icon-leaf blue"></i>
-											</span>
-
-											<span class="input-icon input-icon-right">
-												<input type="text" id="form-field-icon-2" />
-												<i class="icon-leaf green"></i>
-											</span>
-										</div>
-									</div>
-
-									<div class="space-4"></div>
-
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-6">Tooltip and help button</label>
-
-										<div class="col-sm-9">
-											<input data-rel="tooltip" type="text" id="form-field-6" placeholder="Tooltip on hover" title="Hello Tooltip!" data-placement="bottom" />
-											<span class="help-button" data-rel="popover" data-trigger="hover" data-placement="left" data-content="More details." title="Popover on hover">?</span>
-										</div>
-									</div>
-
-									<div class="space-4"></div>
-
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-tags">Tag input</label>
-
-										<div class="col-sm-9">
-											<input type="text" name="tags" id="form-field-tags" value="Tag Input Control" placeholder="Enter tags ..." />
-										</div>
-									</div>
-
-									<div class="clearfix form-actions">
-										<div class="col-md-offset-3 col-md-9">
-											<button class="btn btn-info" type="button">
-												<i class="icon-ok bigger-110"></i>
-												Submit
-											</button>
-
-											&nbsp; &nbsp; &nbsp;
-											<button class="btn" type="reset">
-												<i class="icon-undo bigger-110"></i>
-												Reset
-											</button>
-										</div>
-									</div>
-									
-									<div class="space-24"></div>
-									
-								</form>
+								</div>
+								
+								<div class="space-24"></div>	
+							</form>
 							</div><!-- /.col -->
 						</div><!-- /.row -->
 					</div><!-- /.page-content -->
