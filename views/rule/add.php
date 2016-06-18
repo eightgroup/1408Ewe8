65c1514286ba07216621683f371834a2780e140f
Your Name <381361943@qq.com>
2016-06-18 15:22:40 +0800

    zhaoxin

--- views/rule/add.php
+++ views/rule/add.php
@@ -136,12 +136,8 @@
 									<label class="col-sm-3 control-label no-padding-right" for="form-field-2"> 回复类型 : </label>
 
 									<div class="col-sm-9">
-<<<<<<< HEAD
 										<input type="radio" name="hui" value="1" checked/>关键字回复&nbsp;&nbsp;&nbsp;&nbsp;
                                         <input type="radio" name="hui" value="2"/>全文回复
-=======
-										<input type="text" id="form-field-2" placeholder="回复类型..." class="col-xs-10 col-sm-5" />
->>>>>>> a80cfe56c37a6374a0a522f81ed6c81900f34f16
 									</div>
 								</div>
 
@@ -161,11 +157,7 @@
 									<label class="col-sm-3 control-label no-padding-right" for="form-field-4">添加回复内容 : </label>
 
 									<div class="col-sm-9">
-<<<<<<< HEAD
-										<textarea name="" rows="5" cols="14" id="form-field-3" class="col-xs-10 col-sm-5" placeholder="添加回复内容..."></textarea>
-=======
 										<textarea name="" rows="5" cols="14" id="form-field-2" class="col-xs-10 col-sm-5" placeholder="添加回复内容..."></textarea>
->>>>>>> a80cfe56c37a6374a0a522f81ed6c81900f34f16
 									</div>
 								</div>
 								
@@ -173,11 +165,7 @@
 
 								<div class="clearfix form-actions">
 									<div class="col-md-offset-3 col-md-9">
-<<<<<<< HEAD
 										<button class="btn btn-info" id="button" type="button">
-=======
-										<button class="btn btn-info" type="button">
->>>>>>> a80cfe56c37a6374a0a522f81ed6c81900f34f16
 											<i class="icon-ok bigger-110"></i>
 											Submit
 										</button>
