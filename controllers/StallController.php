d3856c384b3a475299abb068ea52be265b9337d9
Your Name <381361943@qq.com>
2016-06-21 10:07:01 +0800

    小功能

--- controllers/StallController.php
+++ controllers/StallController.php
@@ -10,35 +10,11 @@ class StallController extends \yii\web\Controller{
         return $this->renderPartial('stall');
     }
     public function actionInstall1(){
-        $config=$this->actionDir_writeable('../config');
-        $vendor=$this->actionDir_writeable('../vendor');
-        //echo $config;die;
-        if (is_writable('../config/db.php')) {
-             $db=1;
-            } else {
-              $db=0;
-            }
-        return $this->renderPartial('stall1',array('config'=>$config,'vendor'=>$vendor,'db'=>$db));
+        return $this->renderPartial('stall1');
     }
     public function actionInstall2(){
-        
         return $this->renderPartial('stall2');
     }
-    public function actionDir_writeable($dir) {
-        if(!is_dir($dir)) {
-            @mkdir($dir, 0777);
-        }
-        if(is_dir($dir)) {
-            if($fp = @fopen("$dir/test.txt", 'w')) {
-                @fclose($fp);
-                @unlink("$dir/test.txt");
-                $writeable = 1;
-            } else {
-                $writeable = 0;
-            }
-        }
-        return $writeable;
-      }
     public function actionInstall3(){
         return $this->renderPartial('stall3');
     }
@@ -61,12 +37,10 @@ class StallController extends \yii\web\Controller{
                 echo 0;die;
   
             }else{
-                echo 1;
-            }
-        mysql_query("create database $dbname");
-        $sql=file_get_contents("we2.sql");
-        mysql_select_db($dbname);
-        $a=explode(";",$sql);
+         mysql_query("create database $dbname");
+         $sql=file_get_contents("we2.sql");
+         mysql_select_db($dbname);
+         $a=explode(";",$sql);
      
         foreach ($a as $v) {
             mysql_query($v.';');
@@ -75,21 +49,14 @@ class StallController extends \yii\web\Controller{
    $re=mysql_query($sql4);
       $fh = fopen("../vendor/install.php","w");
       $str="<?php return ['class' => 'yii\db\Connection','dsn' => 'mysql:host=$server;dbname=$dbname','username' => '$username','password' => '$password','charset' => 'utf8',];";
-      file_put_contents('../config/db.php',$str);
+      $file=file_put_contents('../config/db.php',$str);
+      if($fh&&$file){
+         echo 1;
+     }else{
+        echo 0;
+     }
+            }
 
 
-    //     mysqli_query($link,"set name utf8");
-
-    //     //引入sql文件并建表
-    //     $sql=file_get_contents("we2.sql");
-    //     $a=explode(";",$sql);
-    //     // var_dump($a);
-    //     foreach ($a as $v) {
-    //         mysqli_query($link,$v.';');
-    //     }
-           
-    //     //添加管理员!
-    // mysqli_query($link,$sql4);
-   
     }
 }
