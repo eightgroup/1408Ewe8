fcef619017404e9a7fa1dc6d0550aa070cde8d1a
liyajiao <18710127273@163.com>
2016-06-22 08:17:09 +0800

    222

--- config/db.php
+++ config/db.php
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 <?php return ['class' => 'yii\db\Connection','dsn' => 'mysql:host=127.0.0.1;dbname=we8','username' => 'root','password' => 'KOtXA2i7','charset' => 'utf8',];
-=======
-<?php return ['class' => 'yii\db\Connection','dsn' => 'mysql:host=127.0.0.1;dbname=pt','username' => 'root','password' => 'root','charset' => 'utf8',];
->>>>>>> b5f91e6d2965bca9728e8c1b222cb8592f5fdd0c
+
